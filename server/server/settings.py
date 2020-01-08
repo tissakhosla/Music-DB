0941efce8c394b564b2ce0e97e86006ba495b778
Tissa Khosla <tissa.music@gmail.com>
2020-01-08 14:16:37 -0500

    superuser can CRUD data from browser

--- server/server/settings.py
+++ server/server/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'giglog',
+        'USER': 'tissakhosla',
+        'PASSWORD': 'Zaq12wsx',
+        'HOST': 'localhost'
     }
 }
 
