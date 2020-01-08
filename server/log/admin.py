c17a75e64c5158f912f724c949d7f1d344054f5d
Tissa Khosla <tissa.music@gmail.com>
2020-01-08 13:53:09 -0500

    superuser can add simple data to sqlite3 NEXT STEPS:  - switch to Postrsql and then expand data models

--- server/log/admin.py
+++ server/log/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Log
+
+class LogAdmin(admin.ModelAdmin):
+  list_display = ('title', 'dateAndTime')
 
 # Register your models here.
+
+admin.site.register(Log, LogAdmin)
--- server/log/models.py
+++ server/log/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Log(models.Model):
+      title = models.CharField(max_length=120)
+      dateAndTime = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+      def _str_(self):
+        return self.title
